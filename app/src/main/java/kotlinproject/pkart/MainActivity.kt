abeba43a9868bb054cf49f2cdf63506c175e7b3e
Peter Rai <78395050+peterraitamang@users.noreply.github.com>
2023-02-25 20:07:51 +0545

    Initial commit

---
+++ app/src/main/java/kotlinproject/pkart/MainActivity.kt
@@ -0,0 +1,42 @@
+package kotlinproject.pkart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.PopupMenu
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.findNavController
+import kotlinproject.pkart.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragementContainer)
+        val navController = navHostFragment!!.findNavController()
+
+        val popupMenu = PopupMenu(this, null)
+        popupMenu.inflate(R.menu.bottom_nav)
+        binding.bottomBar.setupWithNavController(popupMenu.menu,navController)
+
+        navController.addOnDestinationChangedListener(object : NavController.OnDestinationChangedListener {
+            override fun onDestinationChanged(
+                controller: NavController,
+                destination: NavDestination,
+                arguments: Bundle?
+            ) {
+               title = when(destination.id){
+                   R.id.cartFragment -> "My cart"
+                   R.id.moreFragment -> "My Dashboard"
+                   else -> "P-Kart"
+               }
+            }
+
+
+        })
+    }
+}
